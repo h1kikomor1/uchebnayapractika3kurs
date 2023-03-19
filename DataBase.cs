3fed44421ff591cfacc27965408d051556f8be3f
hikikomor1 <(>
2023-03-19 23:25:37 +0900

    release v1

---
+++ DataBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    class DataBase
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-J7ESJQ5V;Initial Catalog=test;Integrated Security=True");
+
+        public void openConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+            {
+                return con;
+            }
+        
+    }
+}
